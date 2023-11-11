b81055b47aeda8330b4e06e08e73680c22be27ca
4sh <80625238+Recentaly@users.noreply.github.com>
2023-11-11 22:16:01 +0100

    ~

--- modules/models/get_models.py
+++ modules/models/get_models.py
@@ -1,6 +1,8 @@
 # this file returns a list of supported models with type dict
 # example output: {"data": [{"id": "gpt.3-5-turbo"}, {"id": "gpt-4"}]}
 
+# notice the word 'gpt' which is completely out of place? Venus.chub.ai doesn't recognize the model if this keyword isn't included. It's removed before generation but still passed to the site.
+
 async def get_models() -> dict:
 
     return {"data": [
